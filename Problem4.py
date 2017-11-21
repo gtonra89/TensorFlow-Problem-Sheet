69b93acac83ddf3674c0a8b9b319a2508c7cd1f0
Garret Tonra <gtonra89@gmail.com>
2017-11-21 13:21:18 +0000

    updated python file to match jupyter

--- Problem4.py
+++ Problem4.py
@@ -30,3 +30,65 @@ Datacsv["Species"] = Datacsv["Species"].map({
 
 # Converts data to anp matrix
 dataset = Datacsv.as_matrix()
+
+
+
+# Preparing Cross Validation
+
+trainingSet = np.random.randint(dataset.shape[0], size=150)
+testSet = np.random.randint(dataset.shape[0], size=100)
+
+xTrain = dataset[trainingSet, :4]
+yTrain = to_categorical(dataset[trainingSet, 4], num_classes=3)
+
+xTest = dataset[testSet, :4]
+yTest = to_categorical(dataset[testSet, 4], num_classes=3)
+
+
+# Define Model
+
+model = Sequential()  # Creating Sequential Model
+model.add(Dense(32, input_dim=4, activation="relu"))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(3, activation="sigmoid"))
+
+# Compile Model
+# sgd = Stochastic gradient descent optimizer
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=sgd,
+    metrics=['accuracy']
+)
+
+
+# Fit Model
+
+model.fit(
+    xTrain,
+    yTrain,
+    epochs=105,
+    batch_size=10
+)
+
+# 7. Evaluate Model
+
+scores = model.evaluate(xTest, yTest)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+# Save predictions
+
+predictions = pd.DataFrame({
+    "Ids": range(1, 151),
+    "Species": [p.argmax() for p in model.predict(dataset[:, :4])]
+})
+
+predictions["Species"] = Datacsv["Species"].map({
+    0: "Iris-setosa",
+    1: "Iris-versicolor",
+    2: "Iris-virginica"
+}).astype(str)
+
+predictions.to_csv("predictions.csv", index=False)
